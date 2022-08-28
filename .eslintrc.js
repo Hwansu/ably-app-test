3b23fcb2f288a1e2aa37035abe8c3b2cf0d3c257
seohs <>
2022-08-29 00:18:53 +0900

    eslint 설정 작업

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,39 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
-  extends: ['airbnb', 'plugin:prettier/recommended'],
+  extends: ['airbnb', 'plugin:prettier/recommended', 'eslint:recommended'],
+  plugins: ['prettier', 'import'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: ['arrow-function', 'function-expression'] },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
   },
 }
